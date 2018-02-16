f59fcd4104afb94120394e9dcab9b2cb98bb750b
Christopher Watson <christopher_watson@icloud.com>
2018-02-16 10:30:49 -0500

    background

--- assets/script.js
+++ assets/script.js
@@ -4,18 +4,10 @@ var mysteryWordHtml = document.getElementById('mystery-word');
 var lettersGuessedHtml = document.getElementById('letters-guessed');
 var newGameButton = document.getElementById('new-game-button');
 
-//var guessWrong;
-// var wordList = ["chaos", "robot", "house", "thehideout", "kindajazzy", "melodica"];
-// var wrongList = ["-"];
-// var wordBlank = [];
-// var randWord;
-// var count = 0;
-
 
 //hangman object
 var hangman = {
   wordList: ["chaos", "robot", "house", "thehideout", "kindajazzy", "melodica"],
-  // gameOver: false,
   guessWrong: 10,
   wordBlank: [""],
   wrongList: ["-"],
@@ -32,13 +24,13 @@ var hangman = {
   displayWord: function(){
     //choose a word from list and display it on the screen
     this.randWord = this.wordList[Math.floor(Math.random() * this.wordList.length)];
-      console.log("displayWord - randWord: " + this.randWord);
+      // console.log("displayWord - randWord: " + this.randWord);
     for (var i = 0; i < this.randWord.length; i++){
       this.wordBlank[i] = "_";
       };
     this.currWord = this.randWord;
     mysteryWordHtml.textContent = this.wordBlank.join(" ");
-    console.log("displayWord - currWord: " + this.currWord)
+    // console.log("displayWord - currWord: " + this.currWord)
   },
 
   guessLetter: function(a){
@@ -51,7 +43,7 @@ var hangman = {
         lettersGuessedHtml.textContent = this.wrongList.join(" ");
         this.guessWrong--;
         livesHtml.textContent = this.guessWrong;
-        console.log("guessLetter - guessWrong: " + this.guessWrong);
+        // console.log("guessLetter - guessWrong: " + this.guessWrong);
       };
     };
 
@@ -66,27 +58,17 @@ var hangman = {
       };
       mysteryWordHtml.textContent = this.wordBlank.join(" ");
     };
-    
-    //catch all for extra letters
-    // for(var i = 0; i < this.currWord.length; i++){
-
-    // }
-    //   if ( ((this.currWord.includes(a)) && (this.wordBlank[i].includes(this.currWord[i])) ) || ( (!this.currWord.includes(a)) && (this.wrongList.includes(a)) ) ){
-    //     this.this.push(a);
-    //     console.log("guessLetter - thisArray: " + this.this);
-    //   };
-
 
     //if length of current word equals amount of letters swapped, word is complete
     if(this.currWord.length === this.isRight){
       this.isComplete = true;
     };
 
-    console.log("guessLetter - wordBlank: " + this.wordBlank);
-    console.log("guessLetter - currWord: " + this.currWord);
-    console.log("guessLetter - currWordLength: " + this.currWord.length);
-    console.log("guessLetter - isComplete: " + this.isComplete);
-    console.log("guessLetter - isRight: " + this.isRight);
+    // console.log("guessLetter - wordBlank: " + this.wordBlank);
+    // console.log("guessLetter - currWord: " + this.currWord);
+    // console.log("guessLetter - currWordLength: " + this.currWord.length);
+    // console.log("guessLetter - isComplete: " + this.isComplete);
+    // console.log("guessLetter - isRight: " + this.isRight);
   },
 
   newGame: function(){
@@ -108,23 +90,52 @@ var hangman = {
 
   gameOver: function(){
     //if you guessed 10 times, you lose
+    // if(this.guessWrong < 1){
+    //   setTimeout(function() { alert("You Lost"); }, 450);
+    //   setTimeout(function() {
+    //     if(confirm("Would you like to play again?")){
+    //       hangman.newGame();
+    //     };
+    //   }, 500);      
+    // };
+    
     if(this.guessWrong < 1){
-      setTimeout(function() { alert("You Lost"); }, 500);
-      setTimeout(function() {
-        if(confirm("Would you like to play again?")){
-          hangman.newGame();
-        };
-      }, 550);      
-    };
+      $.confirm({
+        title: 'Confirm!',
+        content: 'Simple confirm!',
+        buttons: {
+            confirm: function () {
+                $.alert('Confirmed!');
+            },
+            cancel: function () {
+                $.alert('Canceled!');
+            },
+            somethingElse: {
+                text: 'Something else',
+                btnClass: 'btn-blue',
+                keys: ['enter', 'shift'],
+                action: function(){
+                    $.alert('Something else?');
+                }
+            }
+        }
+      });
+    }
+
+
+
+
+
+
 
     if (this.isComplete){
       //if word is complete, you win
-      setTimeout(function() { alert("You Won!"); }, 500);
+      setTimeout(function() { alert("You Won!"); }, 450);
       setTimeout(function() {
         if(confirm("Would you like to play again?")){
           hangman.newGame();
         };
-      }, 550);      
+      }, 500);      
     };
   },
 
@@ -148,15 +159,15 @@ var hangman = {
 
 newGameButton.addEventListener('click', hangman.newGame);
 
-console.log("wordList: " + hangman.wordList);
-console.log("guessWrong: " + hangman.guessWrong);
-console.log("wordBlank: " + hangman.wordBlank);
-console.log("wrongList: " + hangman.wrongList);
-console.log("count: " + hangman.count);
-console.log("randWord: " + hangman.randWord);
-console.log("newWord: " + hangman.newWord);
-console.log("currWord: " + hangman.currWord);
-console.log("currWordLength: " + hangman.currWord.length);
-console.log("isComplete: " + hangman.isComplete);
-console.log("isRight: " + hangman.isRight);
-console.log("thisArray: " + hangman.this);
+// console.log("wordList: " + hangman.wordList);
+// console.log("guessWrong: " + hangman.guessWrong);
+// console.log("wordBlank: " + hangman.wordBlank);
+// console.log("wrongList: " + hangman.wrongList);
+// console.log("count: " + hangman.count);
+// console.log("randWord: " + hangman.randWord);
+// console.log("newWord: " + hangman.newWord);
+// console.log("currWord: " + hangman.currWord);
+// console.log("currWordLength: " + hangman.currWord.length);
+// console.log("isComplete: " + hangman.isComplete);
+// console.log("isRight: " + hangman.isRight);
+// console.log("thisArray: " + hangman.this);
